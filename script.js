56d52dc41d4232a407d94ea18a738f3d37c8e9ed
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-04-12 10:36:23 -0700

    Disable map interaction

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const map = L.map('map', {scrollWheelZoom: false}).setView([31.5, 35.0], 8);
+const map = L.map('map', {scrollWheelZoom: false, doubleClickZoom: false, zoomControl: false, dragging: false}).setView([31.5, 35.0], 8);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
