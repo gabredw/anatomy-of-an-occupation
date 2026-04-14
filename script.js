bacc9f2d8044819fd03e3339f16c2b2313f07536
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-04-14 11:49:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const isLandscape = window.innerWidth > window.innerHeight;
-const isPhone = window.innerWidth < 768; // typical phone breakpoint
+// const isLandscape = window.innerWidth > window.innerHeight;
+// const isPhone = window.innerWidth < 768; // typical phone breakpoint
 
-const zoomLevel = (isPhone && isLandscape) ? 5 : 8;
+// const zoomLevel = (isPhone && isLandscape) ? 5 : 8;
 
 const map = L.map('map', {
     scrollWheelZoom: false,
     doubleClickZoom: false,
     zoomControl: false,
     dragging: false
-}).setView([31.5, 35.0], zoomLevel);
+}).setView([31.5, 35.0], 7.5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
