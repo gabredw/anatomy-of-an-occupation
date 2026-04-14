1ba8fa8690ed40e7b68344386a138bfbc294f1b7
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-04-14 11:13:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const map = L.map('map', {scrollWheelZoom: false, doubleClickZoom: false, zoomControl: false, dragging: false}).setView([31.5, 35.0], 8);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+const isMobile = window.innerWidth < 768;
 
+const zoomLevel = isMobile ? 6 : 8; // tweak these values
+
+const map = L.map('map', {
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    zoomControl: false,
+    dragging: false
+}).setView([31.5, 35.0], zoomLevel);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
