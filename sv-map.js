59742bddb7bced35b28a9fde8ab82df0ee0c8abf
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-05-13 14:33:59 -0700

    Add SV map slider

--- sv-map.js
+++ sv-map.js
@@ -4,37 +4,52 @@ Papa.parse('data/sv-map-data.csv', {
     complete: function(results) {
         const df = results.data;
 
-        const data = {
-            type: 'scattermap',
-            lon: df.map(item => parseFloat(item.longitude_jitter)),
-            lat: df.map(item => parseFloat(item.latitude_jitter)),
-            marker: {
-                color: 'red',
-                size: 5,
-                opacity: 0.5
-            },
-            customdata: df.map(item => [
-                item.fatalities,
-                item.notes_wrapped
-            ]),
-            hovertemplate: "%{customdata[1]}<br><br>Fatalities: %{customdata[0]}"
+        function yearSlider(year) {
+            const filteredDf = df.filter(item => item.year == year)
+
+            const data = {
+                type: 'scattermap',
+                lon: filteredDf.map(item => parseFloat(item.longitude_jitter)),
+                lat: filteredDf.map(item => parseFloat(item.latitude_jitter)),
+                marker: {
+                    color: 'red',
+                    size: 5,
+                    opacity: 0.5
+                },
+                customdata: filteredDf.map(item => [
+                    item.fatalities,
+                    item.notes_wrapped
+                ]),
+                hovertemplate: "%{customdata[1]}<br><br>Fatalities: %{customdata[0]}"
+            }
+
+            const layout = {
+                map: {
+                    style: 'carto-positron',
+                    center: {lat: 31.888, lon: 35.2},
+                    zoom: 8.7
+                },
+                margin: {t: 0, b: 0, l: 0, r: 0},
+                paper_bgcolor: 'rgba(0,0,0,0)',
+                plot_bgcolor: 'rgba(0,0,0,0)',
+            };
+
+            const config = {
+                scrollZoom: false
+            }
+
+            Plotly.newPlot('sv-map', [data], layout, config);
         }
 
-        const layout = {
-            map: {
-                style: 'carto-positron',
-                center: {lat: 31.95, lon: 35.2},
-                zoom: 8.8
-            },
-            margin: {t: 0, b: 0, l: 0, r: 0},
-            paper_bgcolor: 'rgba(0,0,0,0)',
-            plot_bgcolor: 'rgba(0,0,0,0)',
-        };
+        yearSlider(2016);
 
-        const config = {
-            scrollZoom: false
-        }
+        const slider = document.getElementById('year-slider');
+        const label = document.getElementById('year-label');
 
-        Plotly.newPlot('sv-map', [data], layout, config);
-    }
+        slider.addEventListener('input', function() {
+            label.textContent = this.value;
+            yearSlider(this.value);
+        });
+
+    }   
 })
